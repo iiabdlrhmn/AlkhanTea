816da2a3f47b23aeefb7b8b08bbfb6e60a33f0b5
iiabdlrhmn <rahomikanaan@gmail.com>
2025-10-04 20:04:31 +0300

    finished data fetching

--- js/main.js
+++ js/main.js
@@ -12,5 +12,25 @@ listItems.forEach((item) => {
       div.style.display = "none";
     })
     document.querySelector(item.dataset.cont).style.display = "flex";
+    
+    dataFetch(item.id);
   });
+});
+
+function dataFetch(type="tea") {
+  fetch("assets/data/menu.json")
+  .then(res => res.json())
+  .then(menu => {
+    let productsMenu = document.querySelector(`.${type}`);
+    let products = menu[type]
+    products.forEach((p) => {
+      console.log(p.name);
+    })
+  })
+}
+
+
+// automatically bring the products on the page load  
+window.addEventListener("load", () => {
+  dataFetch()
 });
